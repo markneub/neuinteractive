47a08ff7a039260ae2fba002d0f8ffcdc6092845
Mark Neuburger <markneub@gmail.com>
2019-08-05 13:25:44 -0400

    Update copy

--- pages/index.vue
+++ pages/index.vue
@@ -73,7 +73,7 @@ export default {
         client: 'Siimpl',
         service: 'Back end development',
         stack: 'NodeJS, Express, Jest',
-        description: 'Neu Interactive worked with Seattle-based software consultancy Siimpl on behalf of a client that was behind schedule and struggling with a complicated codebase. We refactored spaghetti code to fix errors and standardize on modern syntax, added comprehensive unit test coverage with Jest to check for logic bugs and avoid regressions, and created and implemented a security plan to prioritize and mitigate critical errors before launch.'
+        description: 'Neu Interactive worked with Seattle-based software consultancy Siimpl on behalf of a client that was behind schedule and struggling with a complicated codebase. We refactored spaghetti code to fix errors and standardize on modern syntax, added comprehensive unit test coverage with Jest to check for bugs and avoid regressions, and created and implemented a security plan to prioritize and mitigate critical errors before launch.'
       }, {
         client: 'Ask Lorem',
         service: 'Front end development',
